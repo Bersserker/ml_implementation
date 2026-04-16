43126c5bf84440419877ea1c2f39ac4d0a251650
Bersserker <bertech@yandex.ru>
2026-04-16 23:24:05 +0300

    readme and ab

--- tests/test_api.py
+++ tests/test_api.py
@@ -10,6 +10,19 @@ header = {'Content-Type': 'application/json', \
 """Reading test batch
 """
 def test_my_api(line):
+    """
+    Предсказание модели.
+    Формат запроса:
+    {
+        features': [90000.0, 2.0, 2.0, 2.0, 34.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 29239.0, 14027.0, 13559.0, 14331.0, 14948.0, 15549.0, 1518.0, 1500.0, 1000.0, 1000.0, 1000.0, 5000.0]
+    }
+
+    Формат ответа (успех):
+    {
+        "prediction": 1,           # класс/значение предсказания (int/float)
+        "probability": 0.92       # вероятность (float, 0–1)
+    }
+    """
     df = pd.read_csv('data/UCI_Credit_Card.csv').drop(columns=['ID','default.payment.next.month'])
     test_sample = df.iloc[line].tolist()
     test_sample = {'features':test_sample}
@@ -20,6 +33,19 @@ def test_my_api(line):
 
 
 def test_rabbit(line):
+    """
+    Предсказание модели.
+    Формат запроса:
+    {
+        features': [90000.0, 2.0, 2.0, 2.0, 34.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 29239.0, 14027.0, 13559.0, 14331.0, 14948.0, 15549.0, 1518.0, 1500.0, 1000.0, 1000.0, 1000.0, 5000.0]
+    }
+
+    Формат ответа (успех):
+    {
+        "prediction": 1,           # класс/значение предсказания (int/float)
+        "probability": 0.92       # вероятность (float, 0–1)
+    }
+    """
     df = pd.read_csv('data/UCI_Credit_Card.csv').drop(columns=['ID','default.payment.next.month'])
     test_sample = df.iloc[line].tolist()
     test_sample = {'features':test_sample}
